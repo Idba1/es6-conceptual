c098e7c623066acd5346dc7240432982bca242a8
Idba1 <moniraislamself@gmail.com>
2024-03-02 03:43:00 +0600

    distructureing

---
+++ distructure.js
@@ -0,0 +1,19 @@
+const person = {
+    name: "Idba",
+    age: 20,
+}
+const showInfo = ({name, age}) => {
+    console.log(name,age);
+}
+showInfo(person);
+
+
+
+/* const person2 = {
+    name: "jani na",
+    age: "jani na",
+}
+const showInfo2 = () => {
+    console.log(person2.name, person2.age)
+}
+showInfo2() */
