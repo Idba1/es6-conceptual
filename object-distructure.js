2df3e0267097eddc500d407fd8d13edb69c9c37d
Idba1 <moniraislamself@gmail.com>
2024-03-01 23:13:00 +0600

    object distructure

---
+++ object-distructure.js
@@ -0,0 +1,11 @@
+const person = {
+    name : "jani na",
+    age : "bolbo na",
+    phone : "sry it's personal",
+    address : "bari nai",
+    p: [1,2,3,4]
+}
+
+const {name, address, age} = person;
+console.log(name);
+console.log(person.p);
