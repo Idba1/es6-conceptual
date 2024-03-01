c05acae0c99a291a1d142101d0eaeff738a07b1b
Idba1 <moniraislamself@gmail.com>
2024-03-01 23:26:02 +0600

    forEach

---
+++ arrayMethods.js
@@ -0,0 +1,15 @@
+/*  1. forEach, 
+    2. map, 
+    3. find, 
+    4. filter, 
+    5. reduce
+ */
+
+
+// forEach
+const numbers = [10, 20, 30, 40, 50, 60, 70];
+numbers.forEach(item => console.log(item+1));
+/* const num = num.forEach(item => {
+    return item+1;
+});
+console.log(num); */
