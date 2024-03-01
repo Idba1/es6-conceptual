c3b2f3a88417708e94e8ffc380e2e21e3d8b0944
Idba1 <moniraislamself@gmail.com>
2024-03-02 02:24:54 +0600

    scope and hoisted

---
+++ scope.js
@@ -0,0 +1,13 @@
+/*
+    scope
+        -global scope
+        -block scope {...}
+        -local scope or function scope //function(){...}
+*/
+
+
+// hoisted
+
+/* console.log(y);
+var y = 0;
+let y = 0; */
